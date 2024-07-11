eb073d3029f7fe1a9a9b9cf629967c6d1a14daaf
Passereau <vorobyev-ivan@yandex.ru>
2024-07-11 16:54:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,9 +34,9 @@
 
                     const text2 = document.createElementNS(xmlns, 'text')
                     text2.setAttributeNS(null, 'x', size / 2 - gap / 2)
-                    text2.setAttributeNS(null, 'y', size / 5 * 3 - gap / 2 - 5)
+                    text2.setAttributeNS(null, 'y', size / 5 * 3 - gap / 2 - 20)
                     text2.setAttribute('style', 'white-space: pre;');
-                    text2.setAttributeNS(null, 'font-size', size / (4 * 2))
+                    text2.setAttributeNS(null, 'font-size', size / (4 + 2))
                     text2.textContent = '99'
 
                     const region = document.createElementNS(xmlns, 'g')
