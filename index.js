82942f0cc9521744b7b1418fe14963847f317f8b
Passereau <vorobyev-ivan@yandex.ru>
2024-07-11 16:11:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,14 @@
                     text.setAttributeNS(null, 'x', size / 2 - gap / 2)
                     text.setAttributeNS(null, 'y', size / 5 * 3 - gap / 2)
                     text.setAttribute('style', 'white-space: pre;');
-                    text.textContent = label+'1'
+                    text.textContent = label+'2'
 
                     const text2 = document.createElementNS(xmlns, 'text')
                     text2.setAttributeNS(null, 'x', size / 2 - gap / 2)
-                    text2.setAttributeNS(null, 'y', size / 5 * 3 - gap / 2)
+                    text2.setAttributeNS(null, 'y', size / 5 * 3 - gap / 2 + 2)
                     text2.setAttribute('style', 'white-space: pre;');
-                    text2.textContent = '88'
+                    region.setAttributeNS(null, 'font-size', size / 4 * 2)
+                    text2.textContent = '77'
 
                     const region = document.createElementNS(xmlns, 'g')
                     region.setAttributeNS(null, 'transform', `matrix(1 0 0 1 ${lon * size} ${lat * size})`)
